d54673734d1ce5b71a32315fef5b142b8dc9574c
saketkumar123 <saket.ulsi@gmail.com>
2019-12-06 09:41:51 +0530

    Added JSP

--- src/main/java/com/devops/demo/myfirstdemo/HelloWorld.java
+++ src/main/java/com/devops/demo/myfirstdemo/HelloWorld.java
@@ -1,13 +1,22 @@
 package com.devops.demo.myfirstdemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorld {
+	
+	public static final String INDEX = "index";
 
     @GetMapping("/hello")
     public void print(){
         System.out.println("Hello World");
+    }
+    
+    @GetMapping("/")
+    public String welcome(ModelMap model) {
+    	return INDEX;
     }
 }
